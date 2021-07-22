310013bd21146cf08ff0d2855dabf4f7832d8a68
HAINAM-7 <user@users-MacBook-Pro.local>
2021-07-23 04:08:03 +0700

    adding header and links to home and shop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 const HatsPage = () => (
   <div>
       <h1>HATS PAGE</h1>
@@ -11,6 +13,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
+      <Header/>
         <Switch>
           <Route  exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} />
