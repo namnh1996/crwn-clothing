334821fe4a8ded64217b381825e5ac7af3280d90
HAINAM-7 <user@users-MacBook-Pro.local>
2021-08-15 02:53:15 +0700

    added addItem and removeItem func in checkout item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,4 +7,14 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
     type: CartAcionType.ADD_ITEM,
     payload: item
+});
+
+export const clearItemFromCart = item => ({
+    type: CartAcionType.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
+
+export const removeItem = item => ({
+    type: CartAcionType.REMOVE_ITEM,
+    payload: item
 })
